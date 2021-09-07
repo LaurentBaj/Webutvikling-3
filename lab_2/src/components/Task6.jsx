f81771d8e2a86a4eb2fdc4c4a0426f45f1a2433b
laurent_ Bajrami <laurent_baj@hotmail.com>
2021-09-07 20:42:39 +0200

    Ex6 Fuckery the sequel

--- lab_2/src/components/Task6.jsx
+++ lab_2/src/components/Task6.jsx
@@ -7,23 +7,13 @@ export function Task6() {
 
     let [movie, setMovie] = useState({}) // 'Singleton'
 
-    function action() {
-        if (movieName && category) {
-            setMovie(movie = { // Replace vals
-                name: movieName,
-                category: category
-            })
-        }
-        setMovieName("")
-        setCategory("")
-    }
 
     return <>
         <h1>Task 6: A movie object, version 2</h1>
         <label>Title</label>
-        <input type="text" value={movieName} onChange={event => setMovieName(event.target.value)} />
+        <input type="text" value={movieName} onChange={event => setMovie(event.target.value)} />
         <label>Category</label>
-        <input type="text" value={category} onChange={event => setCategory(event.target.value)} />
+        <input type="text" value={category} onChange={event => setMovie(event.target.value)} />
         <h3>Title: {movie.name}</h3>
         <p>Category: {movie.category}</p>
     </>
