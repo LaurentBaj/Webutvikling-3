bb0c707a6b0f8afdd185fb964eade67f3475e969
laurent_ Bajrami <laurent_baj@hotmail.com>
2021-09-10 11:52:12 +0200

    Finished Ex6

--- lab_2/src/components/Task6.jsx
+++ lab_2/src/components/Task6.jsx
@@ -5,15 +5,25 @@ export function Task6() {
     let [movieName, setMovieName] = useState()
     let [category, setCategory] = useState()
 
-    let [movie, setMovie] = useState({}) // 'Singleton'
+    let [movie, setMovie] = useState({})
 
+    function changeName(event) {
+        setMovieName(event.target.value)
+        setMovie({...movie, name: movieName})
+    }
+
+    function changeCategory(event) {
+
+        setCategory(event.target.value)
+        setMovie({...movie, category: category})
+    }
 
     return <>
         <h1>Task 6: A movie object, version 2</h1>
         <label>Title</label>
-        <input type="text" value={movieName} onChange={event => setMovie(event.target.value)} />
+        <input type="text" value={movieName} onChange={changeName} />
         <label>Category</label>
-        <input type="text" value={category} onChange={event => setMovie(event.target.value)} />
+        <input type="text" value={category} onChange={changeCategory} />
         <h3>Title: {movie.name}</h3>
         <p>Category: {movie.category}</p>
     </>
