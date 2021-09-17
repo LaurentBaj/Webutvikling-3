228eacf79a3be7cec43e2b29f45dd434ed446e8d
laurent_ Bajrami <laurent_baj@hotmail.com>
2021-09-17 09:16:53 +0200

    Boiler-plate Typescript

---
+++ sports/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {FC} from "react";
+
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>Hollow World!</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ sports/src/routing/Routing.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+
+const Routing: FC = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Routing
